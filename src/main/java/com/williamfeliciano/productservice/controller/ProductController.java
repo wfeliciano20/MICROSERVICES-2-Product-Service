2a5d8b8e8fdb471bd5434e7c51ff7ba309f039bb
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2023-04-24 23:45:58 -0400

    Implemented Security

--- src/main/java/com/williamfeliciano/productservice/controller/ProductController.java
+++ src/main/java/com/williamfeliciano/productservice/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.williamfeliciano.productservice.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,12 +16,14 @@ public class ProductController {
 
     private ProductService productService;
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
     public ResponseEntity<Long> addProduct(@RequestBody ProductRequest productRequest){
         long productId = productService.addProduct(productRequest);
         return new ResponseEntity<>(productId, HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') || hasAuthority('Customer') || hasAuthority('SCOPE_internal')")
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponse> getProductById(@PathVariable("id") long productId){
         ProductResponse productResponse = productService.getProductById(productId);
@@ -28,6 +31,7 @@ public class ProductController {
     }
 
 
+    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('ADMIN')")
     @PutMapping("/reduceQuantity/{id}")
     public ResponseEntity<Void> reduceQuantity(@PathVariable("id") long productId, @RequestParam long quantity){
         productService.reduceQuantity(productId, quantity);
